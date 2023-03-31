f1d7fca233aa62f0c06abbc7c10104cbd16fc001
gene21gene <owens.eugene@yahoo.com>
2023-03-30 21:58:55 -0400

    adding logout.js

---
+++ static/logout.js
@@ -0,0 +1,13 @@
+const logout = document.querySelector(".nav-link.logout");
+
+logout.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  fetch("/logout", { method: "POST" })
+    .then((response) => {
+      window.location.href = "/login";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
