c7588afbb409196e87003965b30b6f0b40f9f983
gene21gene <owens.eugene@yahoo.com>
2023-05-20 08:47:50 -0400

    turning off debug toolbar

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pdb
 import uuid
 
 from flask import Flask, render_template, request, flash, redirect, session, g, abort, jsonify
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 from flask_wtf import FlaskForm
 from wtforms import HiddenField
@@ -26,7 +26,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-toolbar = DebugToolbarExtension(app)
+
+# Only use in debug mode
+if app.debug:
+    from flask_debugtoolbar import DebugToolbarExtension
+    toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
